293baa73c6a72918d7899750bbe7390bec60fe98
chknwngs999 <ryanlee4761@gmail.com>
2021-10-03 14:05:01 -0700

    Changed app setup

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, flash, request, url_for
 #from flaskr.db import get_db
 
-app = Flask(__name__)
+def create_app(test_config = None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    @app.route("/input", methods=("GET", "POST"))
+    def inputpage():
+        if request.method == "POST":
+            given = request.form['inputfield']
+            flash(given)
+        return render_template('input.html')
+
+    return app
+
+app = create_app()
