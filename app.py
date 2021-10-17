02e93556bdd774a2ec67cbc6495aae998f6e67cd
Ryan Lee <ryanlee4761@gmail.com>
2021-10-16 21:32:44 -0700

    Ignore input page, handle pages in app

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, session
-#from flaskr.db import get_db
 
 
 def create_app(test_config=None):
+    foodemissions = [6.62, 2.45, 1.72, 1.26,
+                     0.89, 0.72, 0.16, 0.11, 0.07, 0.03]
+    clothingemissions = [4.6, ]
+    # 2.1 kg co2 cotton, 5.5 kg co2 polyester
+    #4.6, 12.1
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -17,48 +21,113 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
-    @app.route("/inputpage", methods=("GET", "POST"))
-    def inputpage():
-        if request.method == "POST":
-            given = request.form['inputfield']
-            if int(given) >= 500:
-                flash("That's a big number!")
-            else:
-                flash("Tiny. Go bigger.")
-        return render_template('inputpage.html')
-
     @app.route("/foodquestionnaire", methods=("GET", "POST"))
     def foodquestionnaire():
         if request.method == "POST":
-            session['responsedict'] = {
-                'beef': request.form['beef'],
-                'pork': request.form['pork'],
-                'poultry': request.form['poultry'],
-                'cheese': request.form['cheese'],
-                'eggs': request.form['eggs'],
-                'rice': request.form['rice'],
-                'legumes': request.form['legumes'],
-                'carrots': request.form['carrots'],
-                'potatoes': request.form['potatoes'],
-            }
-            return redirect(url_for('results'))
-        session['responsedict'] = {}
+            session['foodlist'] = [
+                request.form['beef'],
+                request.form['pork'],
+                request.form['poultry'],
+                request.form['cheese'],
+                request.form['eggs'],
+                request.form['rice'],
+                request.form['legumes'],
+                request.form['carrots'],
+                request.form['potatoes'],
+            ]
+            return redirect(url_for('foodresults'))
+        session['foodlist'] = {}
         return render_template('foodquestionnaire.html')
+        # fix 2 2 2 2 2 2 2  2 2 bug
+        # round numbers??
 
     @app.route("/foodresults")
-    def results():
-        responsedict = session.get('responsedict', None)
-        # return render_template('results.html')
-        if responsedict != {}:
-            emissions = [6.62, 2.45, 1.72, 1.26,
-                         0.89, 0.72, 0.16, 0.11, 0.07, 0.03]
-            total_co2 = 0
-
-            for i in range(len(emissions)-1):
-                total_co2 += int(list(responsedict.values())[i]) * emissions[i]
-            return render_template('foodresults.html', total_co2=total_co2)
+    def foodresults():
+        foodlist = session.get('foodlist', None)
+        foodco2 = 0
+        if foodlist != {}:
+            for i in range(len(foodemissions)-1):
+                foodco2 += int(foodlist[i]) * foodemissions[i]
+            return render_template('foodresults.html', foodco2=foodco2)
         else:
             return redirect('foodquestionnaire')
+
+    @app.route("/clothingquestionnaire", methods=("GET", "POST"))
+    def clothingquestionnaire():
+        if request.method == "POST":
+            session['clothingdict'] = [
+                request.form['clothing_top'],
+                request.form['clothing_bottom'],
+                request.form['clothing_shoes'],
+                request.form['clothing_outerwear'],
+            ]
+            session['clothingoften'] = int(request.form['clothing_often'])
+            # laundry habits
+
+            return redirect('clothingresults')
+        session['clothingdict'] = {}
+        session['clothingoften'] = None
+        # laundry habits
+        return render_template('clothingquestionnaire.html')
+
+    @app.route("/clothingresults")
+    def clothingresults():
+        clothingdict = session.get('clothingdict', None)
+        clothingoften = session.get('clothingoften', None)
+        clothingco2 = 0
+        if clothingdict != {}:
+            for i in range(len(clothingdict)-1):
+                # * something idk wait for jessica
+                # * clothingemissions[i] but fill in list
+                clothingco2 += int(clothingdict[i]) * clothingoften
+
+            # laundry habits
+            return render_template('clothingresults.html', clothingco2=clothingco2)
+        else:
+            return redirect('clothingquestionnaire')
+
+    @app.route("/utilitiesquestionnaire", methods=("GET", "POST"))
+    def utilitiesquestionnaire():
+        if request.method == "POST":
+            session['kwh_pm'] = int(request.form['kwh_pm'])
+            session['weeklydrive'] = int(request.form['weeklydrive'])
+            session['weeklybus'] = int(request.form['weeklybus'])
+            session['carpoolers'] = int(request.form['carpoolers'])
+            session['busmode'] = request.form['busmode']
+            return redirect('utilitiesresults')
+        session['kwh_pm'] = None
+        session['weeklydrive'] = None
+        session['weeklybus'] = None
+        session['carpoolers'] = None
+        session['busmode'] = None
+        return render_template('utilitiesquestionnaire.html')
+
+    @app.route("/utilitiesresults")
+    def utilitiesresults():
+        kwh_pm = session.get('kwh_pm', None)
+        weeklydrive = session.get('weeklydrive', None)
+        weeklybus = session.get('weeklybus', None)
+        carpoolers = session.get('carpoolers', None)
+        busmode = session.get('busmode', None)
+        busfactor = 0
+        utilitiesco2 = 0
+
+        if not (busmode == None or weeklydrive == None):
+            # fix radio buttons
+            if busmode == "bus":
+                busfactor = 0.64
+            elif busmode == "light":
+                busfactor = 0.36
+            elif busmode == "heavy":
+                busfactor = 0.22
+
+            utilitiesco2 += kwh_pm * 12 * .92
+            utilitiesco2 += weeklydrive * .89 / (carpoolers+1)
+            utilitiesco2 += weeklybus * busfactor
+
+            return render_template('utilitiesresults.html', utilitiesco2=utilitiesco2)
+        else:
+            return redirect('utilitiesquestionnaire')
 
     return app
 
