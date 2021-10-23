8f6aff6f780a1d5aef0e34f45d8d1097b3dba91c
Ryan Lee <ryanlee4761@gmail.com>
2021-10-23 10:54:11 -0700

    Require form section when shown

--- static/form.js
+++ static/form.js
@@ -1,12 +1,24 @@
 function showFormSection() {
     var busmiles = document.getElementById('weeklybus').value;
     const busmode = document.getElementById('busmodeform');
+    const section_bus = document.getElementById('bus');
+    const section_light = document.getElementById('light');
+    const section_heavy = document.getElementById('heavy');
+
     if (busmiles != "" && parseInt(busmiles) != 0) {
         busmode.style.visibility = 'visible';
         busmode.style.fontSize = '16px';
+
+        section_bus.setAttribute('required', "");
+        section_light.setAttribute('required', "");
+        section_heavy.setAttribute('required', "");
     } else if (busmiles == "") {
         busmode.style.visibility = 'hidden';
         busmode.style.fontSize = '3px';
+
+        section_bus.removeAttribute('required', "");
+        section_light.removeAttribute('required', "");
+        section_heavy.removeAttribute('required', "");
     }
 }
 
